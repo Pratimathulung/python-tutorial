b8e8d7d61459aefa0f6d6cc483a4dfb698ebe378
Pratima Rai <pratima.thulungrai@gmail.com>
2018-06-19 16:03:58 -0700

    Updated fibonacci series printing with function

--- python-for-everybody/assignments/fibonnaci_series.py
+++ python-for-everybody/assignments/fibonnaci_series.py
@@ -1,8 +1,23 @@
-# Wap to print the fibonnaci series
+# Wap to print the fibonnaci series calling function that gives you list of fibonaci numbers with
+# max number as input
 
 
-a, b = 0, 1
-while b < 10:
-    print(b)
-    a, b = b, a + b
+# a, b = 0, 1
+# while b < 10:
+#     print(b)
+#     a, b = b, a + b
 
+def fib(number):
+    fib_series = []
+    a, b = 0, 1
+    while b < number:
+        fib_series.append(b)
+        a, b = b, a + b
+
+    return fib_series
+
+
+print(fib(5))
+print(fib(30))
+print(fib(50))
+print(fib(60))
