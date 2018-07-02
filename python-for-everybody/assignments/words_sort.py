b6f81532befab322f808a1bcafbd36799d25e112
Pratima Rai <pratima.thulungrai@gmail.com>
2018-07-02 14:09:16 -0700

    Added example to remove duplicate words

--- python-for-everybody/assignments/words_sort.py
+++ python-for-everybody/assignments/words_sort.py
@@ -8,3 +8,15 @@ def sort_word(words):
 
 words = input("Enter your words here:").split(',')
 print(sort_word(words))
+
+
+# Write a program that accepts a sequence of whitespace separated words as input and prints the words after removing
+#  all duplicate words and sorting them alphanumerically.
+
+def remove_duplicate(words):
+    result = set(words)
+    return ' '.join(sorted(result))
+
+
+words = input('Enter your line here:').split(' ')
+print(remove_duplicate(words))
