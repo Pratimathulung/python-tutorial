3766b05522973d82cd683302350da58e2c16ffe1
Pratima Rai <pratima.thulungrai@gmail.com>
2018-07-11 10:46:11 -0700

    Started chapter 8

---
+++ python-data-structures/chapter8_list/List_and_definite_loops.py
@@ -0,0 +1,8 @@
+friends = ['Monica','Tshering','Eliza']
+for friend in friends:
+    print('Happy friendship day', friend)
+print('Bye :)')
+
+friends[2] = 'Bimala'
+print(friends)
+
---
+++ python-data-structures/chapter8_list/create_empty_list.py
@@ -0,0 +1,11 @@
+stuff = list()
+stuff.append('book')
+print(stuff)
+
+# def make_list(list):
+#     list = []
+#     list.append('book')
+#     return list
+#
+#
+# print(make_list(list))
