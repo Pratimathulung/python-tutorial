868520fc09ab097102e49ac2a1f1a4dbb6211b31
Pratima Rai <pratima.thulungrai@gmail.com>
2018-07-16 11:24:21 -0700

    Added example to compare list and dict

---
+++ python-data-structures/chapter9_dictionary/comparing_list_and_dict.py
@@ -0,0 +1,16 @@
+# List
+
+lst = list()
+lst.append(2)
+lst.append(25)
+print(lst)
+lst[0] = 15
+print(lst)
+
+# Dictionary
+dct = dict()
+dct['name'] = 'Tshering'
+dct['age'] = 23
+print(dct)
+dct['name'] = 'Muna'
+print(dct)
