89af6a63cbeaf2ee9eb7eefb2398870bc1a6fc7f
Pratima Rai <pratima.thulungrai@gmail.com>
2018-07-18 14:41:06 -0700

    Added example for tuples

---
+++ python-data-structures/chapter10_tuples/tuple.py
@@ -0,0 +1,19 @@
+names = ('Glen', 'Sally', 'Joseph')
+print(names[0])
+print(names)
+for name in names:
+    print(name)
+
+numbers = (1, 3, 6)
+print(numbers)
+print(max(numbers))
+print(min(numbers))
+for num in numbers:
+    print(num)
+
+# Tuples are immutable
+names = ('Glen', 'Sally', 'Joseph')
+names[2] = 'Pratima' # fails with traceback
+
+
+
