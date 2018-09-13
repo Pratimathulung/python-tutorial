6dfc25f8a3b5051bd5c79d6bf7b11c2e47d9fa87
Pratima Rai <pratima.thulungrai@gmail.com>
2018-09-13 14:34:26 -0700

    Did assignment of chapter 12

---
+++ using-python-to-access-web-data/sockets_chapter12/assignment_beautifulSoup.py
@@ -0,0 +1,13 @@
+import urllib.request
+from bs4 import BeautifulSoup
+
+page = urllib.request.urlopen(input('Enter URL:'))
+soup = BeautifulSoup(page, 'html.parser')
+spans = soup('span')
+
+numbers = []
+
+for span in spans:
+    numbers.append(int(span.contents[0]))
+
+print(sum(numbers))
